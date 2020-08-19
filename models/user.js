347e5b1ece6c3c0a204ff802cd19a60ec54ed5d4
MinJun Seo <smj0385@naver.com>
2020-08-19 19:00:26 +0900

    docs : define method "comparePassword"
    입력한 비밀번호가 해당 user의 비밀번호와 일치하는지 검사하는 사용자 정의 메소드를 정의하였습니다.
    bcrypt 모듈을 사용하였습니다.

--- models/user.js
+++ models/user.js
@@ -33,6 +33,11 @@ const userSchema = mongoose.Schema({
   }
 });
 
+/*
+mongoose.pre('event', callback)
+pre method를 통해 특정 이벤트 발생 전에
+반드시 이 method의 callback 함수를 실행하게 된다.
+*/
 userSchema.pre('save', function(next) {
   const user = this;
   
@@ -49,8 +54,20 @@ userSchema.pre('save', function(next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+// 사용자 지정 method 정의하기
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err, false);
+    }
+    callback(null, true);
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
