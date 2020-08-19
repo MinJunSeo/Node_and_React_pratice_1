df9b4b4eff029e638c0867203238ee95e90f57fd
MinJun Seo <smj0385@naver.com>
2020-08-19 22:00:52 +0900

    docs : add logout
    logout 기능을 구현하였다.
    user의 _id 값을 통해 해당 user를 찾은 뒤, token 값을 없애주는 방식으로 logout이 진행된다.

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ app.post('/api/users/login', (req, res) => {
 });
 
 app.get('/api/users/auth', auth, (req, res) => {
+  console.log(req.user);
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -75,6 +76,18 @@ app.get('/api/users/auth', auth, (req, res) => {
     role: req.user.role,
     image: req.user.image
   });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: '' }, (err, user) => {
+      if (err) {
+        return res.json({ success: false, err });
+      }
+      return res.status(200).send({
+        success: true
+      });
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
