c267c6d0cb0b1f20e75619d8148164cf55678201
MinJun Seo <smj0385@naver.com>
2020-08-19 20:29:16 +0900

    docs : add login router
    login과 관련된 라우팅을 완료하였습니다.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 const { User } = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -29,5 +31,36 @@ app.post('/register', (req, res) => {
   });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {  // input한 email에 해당하는 user가 존재하지 않는다면
+      return res.json({
+        loginSuccess: false,
+        message: '입력하신 이메일 혹은 비밀번호가 틀렸습니다.'
+      });
+    }
 
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: '입력하신 이메일 혹은 비밀번호가 틀렸습니다.'
+        });
+      }
+
+      // 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+        // 토큰을 쿠키에 저장
+        res.cookie('x_auth', user.token).status(200).json({
+          loginSuccess: true,
+          userID: user._id
+        });
+      })
+    })
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
